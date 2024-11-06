7864814d6bfdbeb7f4680e1d76d0a1d0d67b6a71
VagharshakGasparyan <vvagharshak@gmail.com>
2024-11-06 13:23:16 +0400

    nor commit 3

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,15 @@
 window.addEventListener("load", ()=>{
     let header = document.querySelector("header");
+    let needChange = true;
     window.addEventListener("scroll", (ev)=>{
         let height = Math.max(150 - window.scrollY, 50);
-        if (height >= 50){
+        if (height === 50 && needChange){
+            needChange = false;
             header.style.height = height + "px";
-            console.log("www");        }
+        }
+        if (height > 50){
+            needChange = true;
+            header.style.height = height + "px";
+        }
     });
 });
