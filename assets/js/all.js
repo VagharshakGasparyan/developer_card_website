616daf2fc9c24eefee9bdad950095e59544a6122
vvagh <vvagharshak@gmail.com>
2024-11-07 18:40:32 +0400

    11_07_2

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,18 +5,19 @@ window.addEventListener("load", ()=>{
     let needChange = true;
     window.addEventListener("scroll", (ev)=>{
         let ws = window.scrollY;
+        let ws_min = Math.min(2 * ws, 100);
         let height = Math.max(150 - window.scrollY, 50);
         if (height === 50 && needChange){
             needChange = false;
             header.style.height = height + "px";
-            ex_left.style.transform = "rotate(-" + ws + "deg)";
-            ex_right.style.transform = "rotate(" + ws + "deg)";
+            ex_left.style.transform = "rotate(-" + ws_min + "deg)";
+            ex_right.style.transform = "rotate(" + ws_min + "deg)";
         }
         if (height > 50){
             needChange = true;
             header.style.height = height + "px";
-            ex_left.style.transform = "rotate(-" + ws + "deg)";
-            ex_right.style.transform = "rotate(" + ws + "deg)";
+            ex_left.style.transform = "rotate(-" + ws_min + "deg)";
+            ex_right.style.transform = "rotate(" + ws_min + "deg)";
         }
     });
 });
